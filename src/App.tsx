b5ae966d9c6af3dc464ae2f7e8976b69f957d2e3
Eman Radwan <emanalaa11499@gmail.com>
2025-12-01 15:08:50 +0200

    initialize project with theme colors

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <button className="bg-sky-700 p-3 m-5 text-white font-semibold">
+        movie hub
+      </button>
+    </>
+  );
+}
+
+export default App;
